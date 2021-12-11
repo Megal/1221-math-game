3c78edd8e911a62adae0af1f97265d355264e9c2
Vidadi Sheidaev <vidadi@aytm.com>
2021-12-11 23:49:38 +0300

    fix tutorial numbers

--- main/tutorial_explain/tutorial_numbers/number_3.go
+++ main/tutorial_explain/tutorial_numbers/number_3.go
@@ -1,18 +1,3 @@
-components {
-  id: "number"
-  component: "/main/number/number.script"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
 embedded_components {
   id: "sprite"
   type: "sprite"
