32d557243cc6525bd07c7203c329c04f4c17c666
Svyatoshenko "Megal" Misha <1654478+Megal@users.noreply.github.com>
2021-12-12 04:51:14 +0300

    Added sound

--- main/carrot/carrot.go
+++ main/carrot/carrot.go
@@ -81,3 +81,26 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "sound"
+  type: "sound"
+  data: "sound: \"/main/sound/mixkit-arcade-video-game-bonus-2044.ogg\"\n"
+  "looping: 0\n"
+  "group: \"sfx\"\n"
+  "gain: 0.25\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
