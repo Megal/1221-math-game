e480dd4e2e69b09aec94e685db9b114882eb651d
Svyatoshenko "Megal" Misha <1654478+Megal@users.noreply.github.com>
2021-12-12 06:38:25 +0300

    new level

--- main/loader/loader.go
+++ main/loader/loader.go
@@ -34,6 +34,24 @@ embedded_components {
 embedded_components {
   id: "main_proxy"
   type: "collectionproxy"
+  data: "collection: \"/main/main_level_2.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "main_proxy_level_2"
+  type: "collectionproxy"
   data: "collection: \"/main/main.collection\"\n"
   "exclude: false\n"
   ""
