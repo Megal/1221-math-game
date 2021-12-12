12f925a3c6c4548f4942edbd07d4eb0ff692254f
Svyatoshenko "Megal" Misha <1654478+Megal@users.noreply.github.com>
2021-12-12 07:50:47 +0300

    loading with idle animation

--- main/loader/loader.go
+++ main/loader/loader.go
@@ -67,3 +67,21 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "loading"
+  type: "collectionproxy"
+  data: "collection: \"/main/loading.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
