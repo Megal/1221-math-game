44457e016600e9b58913d38cd464fb3fc7c905f5
Svyatoshenko "Megal" Misha <1654478+Megal@users.noreply.github.com>
2021-12-12 07:56:46 +0300

    idle animation looped

--- main/player/player_idle.go
+++ main/player/player_idle.go
@@ -2,7 +2,7 @@ embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/main/atlases/pixel_man.atlas\"\n"
-  "default_animation: \"idle\"\n"
+  "default_animation: \"idle_loop\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
